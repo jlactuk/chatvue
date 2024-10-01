deca38d6872452cff1e5f5a91a5b2154f852c29b
jlactuk <jlactuk1@gmail.com>
2024-10-01 22:21:18 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ChatWindow from "./components/ChatWindow.vue";
+
+const listUsers = ['user1', 'user2'];
+console.log(listUsers[0])
+</script>
+
+<template>
+  <template v-for="user in listUsers">
+
+    <ChatWindow :user="user"/>
+  </template>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HeaderWindowComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>{{ props.username }}</p>
+
+</template>
+<script setup>
+const props = defineProps({
+    username: String
+})
+</script>
+<style scoped>
+    p {
+        text-align: left;
+        padding: 5px 10px;
+    }
+</style>
